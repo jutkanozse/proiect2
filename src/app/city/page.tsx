f7021a57b9b86bed3e70a19d5a41b89995343529
Iudith Nozse <jutkanuzse@gmail.com>
2023-12-12 19:25:48 +0200

    api

--- src/app/city/page.tsx
+++ src/app/city/page.tsx
@@ -1,3 +1,87 @@
-export default function Home() {
-    return <h1>City</h1>
+'use client'
+
+import {
+  Container,
+  List,
+  ListItem,
+  ListIcon,
+  Center
+} from '@chakra-ui/react'
+
+
+import { 
+  InfoOutlineIcon
+} from '@chakra-ui/icons'
+
+import { useSearchParams } from 'next/navigation';
+
+import {
+  useState,
+  useEffect
+} from 'react'
+ 
+export default function Page() {
+  const searchParams = useSearchParams()
+  const [data, setData] = useState({})
+  const [extraData, setExtraData] = useState({})
+ 
+  useEffect(() => {
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchParams.get('name')}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.results[0])
+      })
+  }, [])
+ 
+  useEffect(() => {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${searchParams.get('latitude')}&longitude=${searchParams.get('longitude')}&current=temperature_2m,wind_speed_10m,relative_humidity_2m`)
+      .then((res) => res.json())
+      .then((data) => {
+        setExtraData(data.current)
+        console.log(data)
+      })
+  }, [])
+
+  return (
+    <>
+      <Container maxW='1000'>
+        <Center>
+          <List spacing={3} mt={50}>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Name: {data.name}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Latitude: {data.latitude}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Longitude: {data.longitude}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Elevation: {data.elevation}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Population: {data.population}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Temp: {extraData.temperature_2m} C
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Wind speed: {extraData.wind_speed_10m} km/h
+            </ListItem>
+            <ListItem>
+              <ListIcon as={InfoOutlineIcon} color='green.500' />
+              Humidity: {extraData.relative_humidity_2m} %
+            </ListItem>
+          </List>
+        </Center>
+      </Container>
+    </>
+  )
   }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,89 @@
-export default function Home() {
-    return <h1>Search</h1>
+'use client'
+
+import {
+  Button,
+  Center,
+  Container,
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Link
+} from '@chakra-ui/react'
+
+import { 
+  ExternalLinkIcon,
+  LinkIcon
+} from '@chakra-ui/icons'
+
+import {
+  useState,
+  FormEvent
+} from 'react'
+
+export default function Page() {
+  const [data, setData] = useState([])
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${formData.get('name')}`)
+    const data = await response.json()
+    setData(data.results);
+  }
+
+  return (
+    <>
+      <Container maxW='1000'>
+        <form onSubmit={onSubmit}>
+          <Center>
+            <Input id='name' name='name' variant='flushed' placeholder='Enter a city' size='lg' mt={10} borderColor='#545454' textAlign='center' w={400} />
+          </Center>
+          <Center>
+            <Button type='submit' variant='outline' colorScheme='teal' mt={5} w={200}>Submit</Button>
+          </Center>
+        </form>
+        {data.length > 0 &&
+          <TableContainer mt={50}>
+            <Table variant='striped' size='sm'>
+              <TableCaption>Data from <Link href='https://open-meteo.com/en/docs/geocoding-api' isExternal>Geocoding API <ExternalLinkIcon mx='2px' /></Link></TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th isNumeric>Latitude</Th>
+                  <Th isNumeric>Longitude</Th>
+                  <Th isNumeric>Elevation</Th>
+                  <Th isNumeric>Population</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+              {data.map(city => 
+                <Tr>
+                  <Td><Link href={`/city?name=${city.name}&latitude=${city.latitude}&longitude=${city.longitude}`}>{city.name} <LinkIcon mx='2px' /></Link></Td>
+                  <Td isNumeric>{city.latitude}</Td>
+                  <Td isNumeric>{city.longitude}</Td>
+                  <Td isNumeric>{city.elevation}</Td>
+                  <Td isNumeric>{city.population}</Td>
+                </Tr>)}
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th isNumeric>Latitude</Th>
+                  <Th isNumeric>Longitude</Th>
+                  <Th isNumeric>Elevation</Th>
+                  <Th isNumeric>Population</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>}
+      </Container>
+    </>
+  )
   }
